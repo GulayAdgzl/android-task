85cca26542e29dd2f83fff0a9883c7b27b90de5c
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-11-23 02:22:13 +0300

    LayoutManager and Adapter configured for the RecyclerView.

--- app/src/main/java/com/android/android_task/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/android_task/ui/home/HomeFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.android.android_task.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.android_task.adapter.CharacterAdapter
 import com.android.android_task.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var adapter:CharacterAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +24,9 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.RecyclerView.layoutManager=LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
+        binding.RecyclerView.adapter=adapter
     }
 
 
