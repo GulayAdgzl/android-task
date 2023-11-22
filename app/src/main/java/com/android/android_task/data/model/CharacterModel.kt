d4f2b8ad9e36ce745d2e3c6d23f9e76afd8d2333
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-11-22 21:02:54 +0300

    Created CharacterModel class for JSON serialization

---
+++ app/src/main/java/com/android/android_task/data/model/CharacterModel.kt
@@ -0,0 +1,27 @@
+package com.android.android_task.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CharacterModel (
+
+    @SerializedName("BusinessUnitKey")
+    val businessUnitKey: String?,
+    @SerializedName("businessUnit")
+    val businessUnit: String?,
+    @SerializedName("colorCode")
+     val colorCode: String?,
+    @SerializedName("description")
+     val description: String?,
+    @SerializedName("isAvailableInTimeTrackingKioskMode")
+     val isAvailableInTimeTrackingKioskMode: Boolean,
+    @SerializedName("parentTaskID")
+     val parentTaskId: String?,
+    @SerializedName("sort")
+    val sort: String?,
+    @SerializedName("task")
+     val task: String?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("wageType")
+    val wageType: String?,
+        )
