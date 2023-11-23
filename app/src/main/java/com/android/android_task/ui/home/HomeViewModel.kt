73e1969f5045b3703371f3808f6e41853dfd656e
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-11-23 15:31:40 +0300

    Implement HomeViewModel for character data fetching and authorization

---
+++ app/src/main/java/com/android/android_task/ui/home/HomeViewModel.kt
@@ -0,0 +1,45 @@
+package com.android.android_task.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.android_task.data.model.CharacterModel
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+) : ViewModel() {
+    private val _statusLiveData = MutableLiveData<CharacterApiStatus<List<CharacterModel>>>()
+    val statusLiveData: LiveData<CharacterApiStatus<List<CharacterModel>>>
+      get() = _statusLiveData
+
+    init {
+        authorizeAndFetchTask()
+    }
+
+    private fun authorizeAndFetchTask() {
+        viewModelScope.launch {
+            try {
+                // Step 1: Authorize
+
+
+                // Step 2: Fetch Tasks
+
+            } catch (e: Exception) {
+
+            }
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
