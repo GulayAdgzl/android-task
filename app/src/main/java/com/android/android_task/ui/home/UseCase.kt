faed6609e70d14630510ffd6a6e8082b6e31af36
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-11-23 16:27:05 +0300

    implement UseCase with TokenManager dependency

--- app/src/main/java/com/android/android_task/ui/home/UseCase.kt
+++ app/src/main/java/com/android/android_task/ui/home/UseCase.kt
@@ -1,8 +1,11 @@
 package com.android.android_task.ui.home
 
 import com.android.android_task.data.Repository
+import com.android.android_task.util.TokenManager
 
 
-class UseCase (private val repository: Repository,) {
+class UseCase (private val repository: Repository,
+               private val tokenManager: TokenManager
+) {
 
 }
