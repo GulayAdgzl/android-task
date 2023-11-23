75ffaf6716ce88d9da6318894977826b9ec3bdd7
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-11-23 16:53:49 +0300

    Implement authorization and task fetching in HomeViewModel

--- app/src/main/java/com/android/android_task/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/android/android_task/ui/home/HomeViewModel.kt
@@ -1,22 +1,24 @@
 package com.android.android_task.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.android_task.data.Repository
 import com.android.android_task.data.model.CharacterModel
+import com.android.android_task.util.Resource
 import com.android.android_task.util.TokenManager
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class HomeViewModel(
     private val useCase: UseCase,
     private val tokenManager: TokenManager,
     private val repository: Repository
 ) : ViewModel() {
-    private val _statusLiveData = MutableLiveData<List<CharacterModel>>()
-    val statusLiveData: LiveData<List<CharacterModel>>
-      get() = _statusLiveData
+    private val _taskLiveData = MutableLiveData<Resource<List<CharacterModel>>>()
+    val taskLiveData: LiveData<Resource<List<CharacterModel>>> = _taskLiveData
 
     init {
         authorizeAndFetchTask()
@@ -31,19 +33,58 @@ class HomeViewModel(
                 tokenManager.saveAccessToken(accessToken)
 
                 // Step 2: Fetch Tasks
-
+                fetchTasks()
             } catch (e: Exception) {
-
+                handleAuthorizationError(e)
             }
         }
     }
 
+    private fun fetchTasks() {
+        viewModelScope.launch {
+            try {
+                useCase().collect { result ->
+                    when (result.status) {
+                        Resource.Status.SUCCESS -> {
+                            _taskLiveData.value = result
+                            result.data?.let { repository.saveData(it) }
+                        }
+                        Resource.Status.ERROR -> handleTaskFetchError(result.message)
+                        Resource.Status.LOADING -> {
+                            // Do nothing for now
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                handleTaskFetchError(e.localizedMessage)
+            }
+        }
+    }
 
+    fun getLocalTasks(): List<CharacterModel> {
+        return repository.getData()
+    }
 
+    private fun handleAuthorizationError(exception: Exception) {
+        if (exception is HttpException) {
+            exception.message?.let {
+                Log.e(Repository::class.simpleName, it)
+            }
+        } else {
+            exception.message?.let {
+                Log.e(Repository::class.simpleName, it)
+            }
+            exception.printStackTrace()
+        }
+    }
+
+    private fun handleTaskFetchError(errorMessage: String?) {
+        Log.e(
+            HomeViewModel::class.simpleName,
+            errorMessage ?: "Unknown error occurred during task fetch"
+        )
+    }
 }
-
-
-
 
 
 
