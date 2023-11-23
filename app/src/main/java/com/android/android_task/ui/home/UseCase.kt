d77641fbb392726b54529e286e594a708cb62dbf
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-11-23 16:35:11 +0300

    implement UseCase invoke function to fetch data

--- app/src/main/java/com/android/android_task/ui/home/UseCase.kt
+++ app/src/main/java/com/android/android_task/ui/home/UseCase.kt
@@ -1,11 +1,31 @@
 package com.android.android_task.ui.home
 
 import com.android.android_task.data.Repository
+import com.android.android_task.data.model.CharacterModel
+import com.android.android_task.util.Resource
 import com.android.android_task.util.TokenManager
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 
+
+const val AUTH_BEARER = "Bearer "
 
 class UseCase (private val repository: Repository,
                private val tokenManager: TokenManager
 ) {
+    operator fun invoke(): Flow<Resource<List<CharacterModel>>> = flow {
+        try {
+            emit(Resource.loading())
+            val authToken = AUTH_BEARER + tokenManager.getAccessToken()
+            val tasks = repository.getItems(authToken)
+            emit(Resource.success(tasks))
+        } catch (e: HttpException) {
+            emit(Resource.error(e.localizedMessage))
+        } catch (e: IOException) {
+            emit(Resource.error("Check your internet connection"))
+        }
+    }
 
 }
